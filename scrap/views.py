53b56ce654c22f0c815e7d5eed8ba7ccb4f9dd5c
Hu3in <sh.danishyar@gmail.com>
2020-08-06 18:37:14 +0430

    divar scrapping done

--- scrap/views.py
+++ scrap/views.py
@@ -1,8 +1,40 @@
+from django.shortcuts import  render
 import requests
 from bs4 import BeautifulSoup
-from django.shortcuts import render
-from requests.compat import quote_plus
+from time import sleep
+
+url = requests.get("https://divar.ir/s/tehran").text
+soup = BeautifulSoup(url, 'lxml')
+
+posts = soup.find_all('div',class_='post-card-item')
+
+
 
 def home(request):
-
-    return render(request, "scrap/home.html", {})
+    final_post = []
+    for post in posts:
+        slug = post.find('a').get('href')
+        try:
+            subtitle = post.find(class_="post-card__title").text
+        except expression as identifier:
+            None
+        try:
+            description = post.find(class_="post-card__description").text
+        except:
+            description = ""
+        time = post.find(class_="post-card__info-label").text
+        try:
+            img_src = post.find('img').get('src')
+        except:
+            img_src = "https://www.vermeer.com.au/wp-content/uploads/2016/12/attachment-no-image-available.png"
+        
+        if post.find(class_="post-card__info-urgent"):
+            urgent = post.find(class_="post-card__info-urgent").text
+        else:
+            urgent = ""
+        final_post.append((img_src,subtitle, description, time,slug, urgent))
+    
+    context = {
+        'final_post':final_post
+    }
+    return render(request, "scrap/home.html", context)
